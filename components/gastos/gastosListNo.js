b15536897730569283a439fd9d976fd6f0d4f11f
Guillermo <guillef33@gmail.com>
2023-12-18 19:33:22 -0300

    fix deploy

--- components/gastos/gastosListNo.js
+++ components/gastos/gastosListNo.js
@@ -1,44 +1,45 @@
+import Link from "next/link";
+
 const getIngresos = async () => {
-    try {
-      const res = await fetch("http://localhost:3000/api/ingresos", {
-        cache: "no-store",
-      });
-  
-      if (!res.ok) {
-        throw new Error("Failed to fetch ingresos");
-      }
-  
-      return res.json();
-    } catch (error) {
-      console.log("Error loading topics: ", error);
+  try {
+    const res = await fetch("http://localhost:3000/api/ingresos", {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch ingresos");
     }
-  };
-  
-  export default async function GastosList() {
-    const { gastos } = await getIngresos();
-    console.log(gastos);
-  
-    return (
-      <>
-        {gastos.map((t) => (
-          <div
-            key={t._id}
-            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
-          >
-            <div>
-              <h2 className="font-bold text-2xl">{t.titulo}</h2>
-              <div>{t.categoria}</div>
-              <h3>{t.monto}</h3>
-              <Link
-                  href={`gastos/editarGasto/${item.id}`}
-                  className="rounded bg-blue-700 text-white p-2"
-                >
-                  Editar gasto
-                </Link>
-            </div>
-          </div>
-        ))}
-      </>
-    );
+
+    return res.json();
+  } catch (error) {
+    console.log("Error loading topics: ", error);
   }
-  
+};
+
+export default async function GastosList() {
+  const { gastos } = await getIngresos();
+  console.log(gastos);
+
+  return (
+    <>
+      {gastos.map((t) => (
+        <div
+          key={t._id}
+          className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
+        >
+          <div>
+            <h2 className="font-bold text-2xl">{t.titulo}</h2>
+            <div>{t.categoria}</div>
+            <h3>{t.monto}</h3>
+            <Link
+              href={`gastos/editarGasto/${item.id}`}
+              className="rounded bg-blue-700 text-white p-2"
+            >
+              Editar gasto
+            </Link>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+}
